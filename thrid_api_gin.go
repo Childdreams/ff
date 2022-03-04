23cd4943b2ec40a7f5b7df0f1ae688c7b301711f
bao <243790881@qq.com>
2022-03-04 10:06:31 +0800

    tt

--- thrid_api_gin.go
+++ thrid_api_gin.go
@@ -1,21 +1,34 @@
 package ThirdApi_Gin
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type ThridInteraction interface {
 	First(c *gin.Context)
 }
 
-//type ThirdSx struct {
-//}
-//
-//func (t *ThirdSx) First(c *gin.Context) {
-//	c.JSON(http.StatusOK, gin.H{
-//		"msg": "OK",
-//	})
-//}
+type ThirdSx struct {
+}
+
+type FirstReq struct {
+	Content string `json:"content"`
+	Id      string `json:"id"`
+}
+
+type AA struct {
+}
+
+func (t *ThirdSx) First(c *gin.Context) {
+	tt := FirstReq{
+		Content: "123",
+		Id:      "123",
+	}
+	AA.M(c, tt)
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "OK",
+	})
+}
 func ThirdApi(r *gin.Engine, t ThridInteraction) {
 	r.GET("/First-API", t.First)
 }
